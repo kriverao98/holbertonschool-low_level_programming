43cbd9da0160d2d7262c1713fbacaff4364a67df
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-14 15:16:53 -0400

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h> 
 int main ()
 	{
-			//this programm outputs "Programming is like... and ends with the value of 0
+		//this programm outputs "Programming is like... and ends with the value of 0
+		
 		char str1[] = "\"Programming is like building a multilingual puzzle";
 		puts(str1);
 		return 0;
