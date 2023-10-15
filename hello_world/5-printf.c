8febf87352a919539ee288d1f34d00c9880b9513
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-14 21:17:41 -0400

    task5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-/** 
+/**
  * function - main
  * description - simple code that outputs a string
  **/
 
-int main ()
+int main(void)
 
 {
-	printf("with proper grammar,"
-		   	"but the outcome is a piece of art,\n");
-		return 0;
+	printf("with proper grammar,but the outcome is a piece of art,\n");
+	return 0;
 }
