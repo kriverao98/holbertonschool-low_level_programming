7750dc87804e37512c334547c5dfcf118cc1b894
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-14 22:00:07 -0400

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,12 @@
-#include<stdio.h> 
+#include <stdio.h>
+/**
+ * main - prints a string followed by a new line using puts
+ * Description: Simple code that prints out a string
+ * Return: Always 0 (Success)
+ */
 
-int main()
+int main(void)
 {
-	//C program that prints"Programming is like building a multilingual puzzle, followed by a new line.
 	char str1[] = "\"Programming is like building a multilingual puzzle\n";
 	puts(str1);
 	return 0;
