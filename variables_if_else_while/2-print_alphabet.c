16d3bdb047f3fa47bc7f9c48edbf53f473047b4d
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-18 09:24:25 -0400

    task2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A simple code that prints out the alphabet in lowercase, followed by a newline.
+ * Return - Always 0 (Success)
+ */
+
+int main(void)
+{
+	char az = 'a';
+
+	for (az = 'a'; az <= 'z'; az++)
+		
+		putchar(az);
+		{
+			putchar('\n');
+		}
+
+	return (0);
+}
