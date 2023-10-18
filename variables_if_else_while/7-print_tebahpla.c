de09f06dad7d69301f576deb5e3b19798c38f39d
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-18 15:34:24 -0400

    task7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return - Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char za;
+
+	for(za = 'z'; za > 'a'; za--)
+
+	putchar(za);
+
+	putchar('\n');
+
+	return 0;
+}
