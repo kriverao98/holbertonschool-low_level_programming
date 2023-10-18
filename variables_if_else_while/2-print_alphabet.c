c476d61c65f987b83f8c95f7052f2acb75078edb
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-18 09:48:03 -0400

    task2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Description: A simple code that prints out the alphabet 
+ * main - entry point
+ * Description: A simple code that prints out the alphabet
  * in lowercase, followed by a newline.
  * Return - Always 0 (Success)
  */
