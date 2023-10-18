8077de0c698d5b7f81a391359cec302610ec5e00
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-18 14:49:24 -0400

    task5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -5,16 +5,16 @@
  * Return: Always 0 (Success)
  */
 
-int main(void);
+int main(void)
 {
-int a;
+	int a;
 
-for (a = 0; a <= 9; a++);
+	for (a = 0; a <= 9; a++)
+	putchar(a + '0');
+	{
+		putchar('\n');
+	}
 
-{
-	printf("%d\n");
-}
-
-return (0);
+	return (0);
 
 }
