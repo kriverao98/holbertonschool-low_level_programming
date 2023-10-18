ce0da62494e176e3ab2bb9c5c635a078a8478329
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-18 10:24:09 -0400

    task3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -2,19 +2,19 @@
 /**
  * main - A simple program that prints out the alphabet in
  * lowercase and uppercase followed by a newline.
- * Return: Always 0 (Success)
+ * Return: Always 0(Success)
  */
 
 int main(void)
 {
 	char az;
 {
-	for (az = 'a'; az <= 'z'; az++);
+	for (az = 'a'; az <= 'z'; az++)
 	putchar(az);
-	{
-	for (az = 'A'; az <= 'Z'; az++);
+{
+	for (az = 'A'; az <= 'Z'; az++)
 	putchar(az);
-	}
+}
 	{
 		putchar('\n');
 	}
