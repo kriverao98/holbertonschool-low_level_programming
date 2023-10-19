cdff8b5b0b3fb707bb2185fca1860e7d2f70ace2
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-18 21:22:16 -0400

    task8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -5,3 +5,22 @@
  * followed by a newline.
  * Return: Always 0 (Success)
  */
+int main (void)
+{
+	int y;
+	int x;
+
+	for (y = 0; y <= 9; y++)
+	{
+		putchar(y + '0');
+	}
+
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
