9f6b05c1e708aa62bfcaed9497345d204811c403
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-19 12:58:35 -0400

    task1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * entry point - simple program that prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char x;
+		for (x = 'a'; x <= 'z'; x++)
+
+			putchar(x);
+			{
+				putchar('\n');
+			}
+}
+
+int main(void)
+{
+	print_alphabet();
+	
+	return (0);
+}
