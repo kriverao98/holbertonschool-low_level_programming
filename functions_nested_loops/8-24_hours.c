8678aa90cf1b8619e02f3ed7e3222ee38c1e0629
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-20 10:07:21 -0400

    task8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -5,14 +5,14 @@
  * jack_bauer - print every minute of the day,
  * starting from 00:00 to 23:59
  * @c: the parameter to be checked
- * Return: c (Success)
+ * Return: a, b, c, d (Success)
  */
 
 void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a= 0; a <= 2; a++)
+	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
