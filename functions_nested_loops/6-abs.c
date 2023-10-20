74607ec1dd194b34755c1144b1c75a529b0a324f
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-20 08:42:08 -0400

    task6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - this program computes the absolute value of an int
+ * Return: ALways 0 (Success)
+ */
+
+int _abs(int x)
+{ 
+	if (x < 0)
+	{
+		int abs;
+		abs = x * -1;
+		return (abs);
+	}
+
+	return (x);
+}
