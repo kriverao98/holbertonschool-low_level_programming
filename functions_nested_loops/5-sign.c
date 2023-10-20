c8c0ad725edd13d106eb4f7b75dd3d5df7fb3865
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-20 00:26:10 -0400

    task5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign(int n) - this program prints the sign of a number
+ * @n: the parameter to be checked
+ * Return: 1 and prints + if n is greater than zero,
+ * 0 and prints - if n is zero,
+ * -1 and prints - if n is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n >0)
+	{
+		_putchar(43);
+		return (1);
+	}
+
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
