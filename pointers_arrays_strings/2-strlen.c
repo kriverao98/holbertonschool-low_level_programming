cca1e2d6b312c01dd63f63e10b634479f6356729
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-23 12:10:58 -0400

    task2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - This function returns the length of a string.
+ * @s: Variable to be checked
+ * Return: Lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int str;
+
+	str = strlen(s);
+
+	return (str);
+}
