3dfc88a64c734a288d27a752b5058f09f51cbdf6
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-25 19:42:11 -0400

    task8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -3,12 +3,16 @@
 
 void print_array(int *a, int n)
 {
-	int x = 0;
+	int x;
 
-	while (x < n)
+	for (x = 0; x < n; x++)
 	{
-		printf("%d, ", a[x] - 1);
-		x++;
+		printf("%d", a[x]);
+
+		if (x != (n - 1))
+		{
+			printf(", ");
+		}
 	}
 
 	printf("\n");
