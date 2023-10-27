19c4dfcb89f49871b12ac51dad0e9be77d44b512
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-27 13:58:18 -0400

    task2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * _strncpy - Fucntion copies a string
+ * @n: Counter var
+ * @src: Source var
+ * @dest: Destination variable
  */
 
 char *_strncpy(char *dest, char *src, int n)
