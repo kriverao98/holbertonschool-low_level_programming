72f2ef419dd23ff7038afa6ef3b78dc2289f276e
kriverao98 <riveraocasiokoral29@gmail.com>
2023-10-27 09:55:01 -0400

    task0, changed 0-main

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strcat - write a function that concatenates two strings
- * @in: index variable
- * @len: iteration variable
+ * @dest: appended string
+ * @src: string to be appended
  * Return: dest (Success)
  */
 
@@ -13,14 +14,16 @@ char *_strcat(char *dest, char *src)
 	int in = 0;
 	int len = 0;
 
-	while(dest[in++])
+	while (dest[in++])
 	{
 		len++;
 	}
 
 	for (; src[in]; in++)
-	{
 		dest[len++] = src[in];
+
+	{
+		printf("\n");
 	}
 
 	return (dest);
