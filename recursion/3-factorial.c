be5190509b33a29a71f85197c276e35e871ed394
kriverao98 <riveraocasiokoral29@gmail.com>
2023-11-01 14:16:53 -0400

    task3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -9,17 +9,16 @@
 int factorial(int n)
 
 {
-	int factorial(int n)
+if (n <= 0)
+
 {
-	int result = n;
+	return (-1);
+}
 
-	if (n < 0)
-		return (-1);
+else if (n >= 0 && n <= 1)
+{
+	return (1);
+}
 
-	else if (n >= 0 && n <= 1)
-		return (1);
-
-	result *= factorial(n - 1);
-
-	return (result);
+return n * factorial(n - 1);
 }
