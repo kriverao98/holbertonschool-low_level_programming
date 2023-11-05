1a45dbe502b4097996b6be0d67e561d94a5d02eb
kriverao98 <riveraocasiokoral29@gmail.com>
2023-11-05 18:49:03 -0400

    task1, number of argc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print argc
+ * @argc: number of arguments
+ * @argv: unused var
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
