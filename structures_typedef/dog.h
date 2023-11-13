183119fc85e6a4b8d0f14c87994bc4ed10521f0d
kriverao98 <riveraocasiokoral29@gmail.com>
2023-11-13 13:59:56 -0400

    task0, fixed betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,14 +1,17 @@
-#include <stdlib.h>
+#ifndef _DOG_H
+#define _DOG_H
 
 /**
- *
- *
- *
+ * struct dog - new struct
+ * @name: Member to store name
+ * @age: Member to store age
+ * @owner: Member to store owner
  */
-
 struct dog
 {
   char *name;
   float age;
   char *owner;
 };
+
+#endif /* _DOG_H */
