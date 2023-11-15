3ca2296a851c85d3d1c12ee5f13b8b4f66036349
kriverao98 <riveraocasiokoral29@gmail.com>
2023-11-15 14:19:02 -0400

    task4, new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -19,7 +19,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	pup=malloc(sizeof(dog_t));
+	pup = malloc(sizeof(dog_t));
 
 	if (pup == NULL)
 		return (NULL);
