556e2a9cb76f902a365b130b13d846f3115c04b0
kriverao98 <riveraocasiokoral29@gmail.com>
2023-12-02 00:29:34 -0400

    task2, added strlen to fix output issue

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -28,12 +28,15 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->str = strdup(str);
 
 	if (new_node->str == NULL)
+	{
 		free(new_node);
 		return (NULL);
+	}
 
-		new_node->next = *head;
+	new_node->len = strlen(str);
+	new_node->next = *head;
 
-		*head = new_node;
+	*head = new_node;
 
-		return (new_node);
+	return (new_node);
 }
