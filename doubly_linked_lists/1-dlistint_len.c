8ed925d806b8be9d737a92e7ca1a58addcd3f7d1
kriverao98 <riveraocasiokoral29@gmail.com>
2023-12-03 00:47:03 -0400

    task1 and task2, return # of elements and add node to beggining of list

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t node_count = 0;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		node_count++;
+	}
+	return (node_count);
+}
