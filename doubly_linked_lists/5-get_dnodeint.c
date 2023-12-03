e5cd40816ec7657bb90d09728010926ae2219223
kriverao98 <riveraocasiokoral29@gmail.com>
2023-12-03 10:09:20 -0400

    task5, fixed space in betty code

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 #include <stdio.h>
 
-**
- * get_dnodeint_at_index - Returns the nth node of a dlistint_t linked list.
- * @head: Pointer to the head of the doubly-linked list.
- * @index: Index of the node to retrieve, starting from 0.
- *
- * Return: Pointer to the nth node, or NULL if the node does not exist.
- */
+/**
+* get_dnodeint_at_index - Returns the nth node of a dlistint_t linked list.
+* @head: Pointer to the head of the doubly-linked list.
+* @index: Index of the node to retrieve, starting from 0.
+*
+* Return: Pointer to the nth node, or NULL if the node does not exist.
+*/
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
