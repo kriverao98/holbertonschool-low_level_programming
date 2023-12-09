5cef31d015e01014c5f07bf1d94cc8612cca88a4
kriverao98 <riveraocasiokoral29@gmail.com>
2023-12-09 00:28:01 -0400

    task1, reversed last code before checker failed fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -6,6 +6,13 @@
 #include <string.h>
 #include <sys/stat.h>
 
+/*
+* FILE_PERMISSIONS - Macro representing file permissions
+* (S_IRUSR | S_IWUSR)
+*/
+
+#define FILE_PERMISSIONS (S_IRUSR | S_IWUSR)
+
 /**
 * create_file - Creates a file and writes content to it.
 * @filename: Name of the file to create.
@@ -13,8 +20,6 @@
 *
 * Return: 1 on success, -1 on failure.
 */
-
-#define FILE_PERMISSIONS (S_IRUSR | S_IWUSR)
 
 int create_file(const char *filename, char *text_content)
 {
@@ -27,21 +32,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, FILE_PERMISSIONS);
 	if (fd == -1)
 	{
-		if (chmod(filename, FILE_PERMISSIONS | S_IRUSR) == -1)
-		{
-			perror("chmod");
-			return (-1);
-		}
-		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, FILE_PERMISSIONS);
-		if (fd == -1)
-		{
 		perror("open");
 		return (-1);
-		}
-		else {
-			perror("open");
-			return (-1);
-		}
 	}
 
 	if (text_content != NULL)
@@ -56,7 +48,11 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 
-	close(fd);
+	if (close(fd) == -1)
+	{
+		perror("close");
+		return (-1);
+	}
 	return (1);
 }
 
