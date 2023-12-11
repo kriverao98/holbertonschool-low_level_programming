de3787a00b97d40aaab64ed55b3b9eb8faf44262
kriverao98 <riveraocasiokoral29@gmail.com>
2023-12-10 20:48:54 -0400

    task3, this program copies the content of a file to another file

---
+++ file_io/3-cp.c
@@ -0,0 +1,95 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * main - function that copies the content of a file to another file.
+ * @argc: parameter used for arguments.
+ * @argv: arguments passed.
+ * Return: copied file.
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, text, lamp, garage, door;
+	char *pbuffer = NULL;
+
+	if (argc != 3)
+		checkar(argc);
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+		openfail(argv[1]);
+	pbuffer = malloc(1024);
+	if (pbuffer == NULL)
+		return (-1);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+		writefail(argv[2]);
+	while ((text = read(file_from, pbuffer, 1024)) != 0)
+	{
+		if (text == -1)
+			openfail(argv[1]);
+		lamp = write(file_to, pbuffer, text);
+		if (lamp == -1)
+			writefail(argv[2]);
+		else if (lamp == 1024)
+			{
+			free(pbuffer);
+		pbuffer = malloc(1024);
+			if (pbuffer == NULL)
+				return (-1);
+		file_to = open(argv[2], O_WRONLY | O_APPEND);
+		if (file_to == -1)
+			writefail(argv[2]);
+			}
+	}
+	garage = close(file_to);
+	if (garage == -1)
+		closefail(garage);
+	door = close(file_from);
+	if (door == -1)
+		closefail(door);
+	free(pbuffer);
+	return (0);
+}
+/**
+ * openfail - prints string and exits if open fails.
+ * @s: parameter of type char passed to the function for printing.
+ */
+void openfail(char *s)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
+	exit(98);
+}
+/**
+ * writefail - prints string and exits if write fails.
+ * @s: parameter of type char passed to the function.
+ */
+void writefail(char *s)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
+	exit(99);
+}
+/**
+ * closefail - prints string and exits if open fails.
+ * @n: parameter of type char passed to the function for printing.
+ */
+void closefail(int n)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", n);
+	exit(100);
+}
+/**
+ * checkar - function checks arguments.
+ * @n: parameter passed for function.
+ */
+void checkar(int n)
+{
+	if (n != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+}
