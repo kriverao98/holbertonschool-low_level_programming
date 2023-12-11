34436ba84512ef4ec1949cdddfd1e3ba11c80299
kriverao98 <riveraocasiokoral29@gmail.com>
2023-12-10 23:34:47 -0400

    task1, changed file permissions

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, FILE_PERMISSIONS);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, FILE_PERMISSIONS);
 	if (fd == -1)
 	{
 		perror("open");
